3f1d8f39d88cb9cdf928a1744b384ed6ec97a71e
André Vaz <andre.vaz1411@gmail.com>
2024-09-09 13:09:33 +0100

    disable no-empty-object-type in lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,7 @@ const config = {
   ],
   rules: {
     "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-empty-object-type": "off",
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/consistent-type-imports": [
